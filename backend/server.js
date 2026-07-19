082c55b520316628a532c2abddfbfa831b5a45a7
Rishik Sinha <rishiksinha59@gmail.com>
2026-07-19 18:31:57 +0530

    fix: remove trailing whitespace in userRoute import path

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-import userRouter from './routes/userRoute.js '
+import userRouter from './routes/userRoute.js'
 import 'dotenv/config.js'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
