3d66aee5aec24247f38d62467f87a90eda8d256b
Rishik Sinha <rishiksinha59@gmail.com>
2026-07-19 18:11:38 +0530

    config: prepare project files for multi-service Vercel deployment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Add from './pages/Add/Add'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url='http://localhost:4000'
+  const url = import.meta.env.PROD ? "" : "http://localhost:4000"
   return (
     <div>
       <ToastContainer/>
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+
+const basename = import.meta.env.PROD ? "/admin" : "/";
+
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+<BrowserRouter basename={basename}>
     <App />
 </BrowserRouter>
 )
